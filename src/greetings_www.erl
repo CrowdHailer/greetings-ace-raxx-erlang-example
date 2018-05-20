5cf1a53eeaebeacef73f2639f9c51ebc17164f0d
CrowdHailer <peterhsaxton@gmail.com>
2018-05-20 18:27:08 +0100

    user erlang friendly raxx module

--- src/greetings_www.erl
+++ src/greetings_www.erl
@@ -5,5 +5,5 @@
 -export ([handle_head/2]).
 
 handle_head(_Request, _Config) ->
-  Response = 'Elixir.Raxx':response(ok),
-  'Elixir.Raxx':set_body(Response, <<"Hello, World">>).
+  Response = raxx:response(ok),
+  raxx:set_body(Response, <<"Hello, World">>).
