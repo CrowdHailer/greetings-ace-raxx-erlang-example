0fbbf83b8b92c094a68b8e1e036091efbc80042c
CrowdHailer <peterhsaxton@gmail.com>
2018-05-20 19:03:21 +0100

    handle all streaming callbacks

--- src/greetings_www.erl
+++ src/greetings_www.erl
@@ -2,8 +2,31 @@
 
 -behaviour('Elixir.Raxx.Server').
 
--export ([handle_head/2]).
+-export ([
+  handle_head/2,
+  handle_data/2,
+  handle_tail/2,
+  handle_request/2,
+  handle_info/2
+]).
 
-handle_head(_Request, _Config) ->
+handle_request(_Request, _State) ->
   Response = raxx:response(ok),
   raxx:set_body(Response, <<"Hello, World">>).
+
+handle_info(Info, State) ->
+  error_logger:info_msg("~p~n", [Info]),
+  {[], State}.
+
+handle_head(Request0 = #{body := false}, State) ->
+  Request1 = Request0#{body := <<"">>},
+  handle_request(Request1, State);
+handle_head(Request = #{body := true}, State) ->
+  {[], {Request, <<"">>, State}}.
+
+handle_data(Data, {Request, Buffer, State}) ->
+  {[], {Request, <<Buffer/binary, Data/binary>>, State}}.
+
+handle_tail([], {Request0, Body, State}) ->
+  Request1 = Request0#{body := Body},
+  handle_request(Request1, State).
